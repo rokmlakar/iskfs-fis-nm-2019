ad71d641a974f86c75d1dd877ae4c2d895cfc5c6
rokmlakar <roky.mlakar@gmail.com>
2019-02-26 19:55:03 +0000

    tretji primer

--- Primer03.js
+++ Primer03.js
@@ -2,49 +2,44 @@ var http = require("http");
 var firmata = require("firmata");
 
 var board = new firmata.Board("/dev/ttyACM0", function(){// ACM (Abstract Control Model)
-                                                         // za serijsko komunikacijo z Arduinom (lahko je USB)
+     
     console.log("Priklop na Arduino");
-    board.pinMode(8, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod
+    board.pinMode(8, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod                                                    // za serijsko komunikacijo z Arduinom (lahko je USB)
     board.pinMode(13, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod
-    board.pinMode(2, board.MODES.OUTPUT);
+
+    
 });
-console.log("Zagon aplikacije");
 
 http.createServer(function(req, res){ // http.createServer([requestListener])
                                       // "requestListener" je funkcija, ki se avtomatsko doda
                                       // k dogodku 'request'.
     var deli = req.url.split("/"), // razdelimo url glede na znak "/"
     operator = parseInt(deli[1],10); // 10 osnova številskega sistema, dacimalno (od 2 do 36)
-        
-    if (operator == 0) {
+    var operator1 = parseInt(deli[1],10); // 10 osnova številskega sistema, decimalno (od 2 do 36)
+    var operator2 = parseInt(deli[2],10); // 10 osnova številskega sistema, decimalno (od 2 do 36)
+
+    if (operator1 == 0) {
         console.log("Izključevanje LED1");
         board.digitalWrite(13, board.LOW);
     }
-    if (operator == 1) {
+    if (operator1 == 1) {
         console.log("Vključevanje LED1");
         board.digitalWrite(13, board.HIGH);
     }
     
-    if (operator == 2) {
+    if (operator2 == 0) {
         console.log("Izključevanje LED2");
         board.digitalWrite(8, board.LOW);
     }
 
-    if (operator == 3) {
+    if (operator2 == 1) {
         console.log("Vključevanje LED2");
         board.digitalWrite(8, board.HIGH);
-    }       
-    if (operator == 4) {
-        console.log("Izključevanje LED2");
-        board.digitalWrite(2, board.LOW);
-    }
-
-    if (operator == 5) {
-        console.log("Vključevanje LED2");
-        board.digitalWrite(2, board.HIGH);
-    }               
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    res.write("Za test vpišite v brskalnikovo vrstico z naslovom: http://192.168.1.205:8080/1 <br>");
-    res.write("ali: http://192.168.1.205:8080/0<br>");
-    res.end("Vrednost operatorja: " + operator);
+    } 
+   
+        
+     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});   
+    res.write("Za test vpišite v brskalnikovo vrstico z naslovom: http://192.168.1.205:8080/1/1 <br>");
+    res.write("ali: http://192.168.1.205:8080/0/0<br>");
+    res.end("Vrednost operatorja1: " + operator1 + "<br>" + "Vrednost operatorja2: " + operator2);
 }).listen(8080, "192.168.1.205");
